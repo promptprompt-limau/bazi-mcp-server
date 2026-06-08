6f411cfaa8a77c0589df1ab49b9f62d02a8ac8a7
promptprompt-limau <jydarrell@gmail.com>
2026-06-08 21:33:16 +0800

    update to 10mb

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({ origin: '*' }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 let baziModule = null;
 
